23d779a86ea7cd7538f6609b4d68de3a6f0dd8ef
Jason Levinsohn <jason.levinsohn@gmail.com>
2014-07-03 12:20:51 -0400

    updated test result locations

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function(config) {
         singleRun: true,
         reporters: ['dots', 'junit'],
         junitReporter: {
-           outputFile: 'test-results.xml' 
+           outputFile: 'test/test-results.xml' 
         }
 	});
 };
